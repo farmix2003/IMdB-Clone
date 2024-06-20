d139efb898a396c7eba46126954ad4e781d1c52d
Farmix2003 <ffarrux386@gmail.com>
2024-06-20 14:36:29 +0300

    About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 sapce-y-4">
+      <h1 className="text-2xl font-meduim text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ab nesciunt
+        illum tempora commodi doloribus sapiente repudiandae ipsum optio
+        temporibus ea recusandae delectus magni similique, deserunt odio
+        numquam, inventore ad.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ab nesciunt
+        illum tempora commodi doloribus sapiente repudiandae ipsum optio
+        temporibus ea recusandae delectus magni similique, deserunt odio
+        numquam, inventore ad.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ab nesciunt
+        illum tempora commodi doloribus sapiente repudiandae ipsum optio
+        temporibus ea recusandae delectus magni similique, deserunt odio
+        numquam, inventore ad.
+      </p>
+    </div>
+  );
+};
+
+export default About;
