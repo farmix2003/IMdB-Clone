6619748434ae5ac3d33657267f3c4fee22551016
Farmix2003 <ffarrux386@gmail.com>
2024-07-28 18:02:43 +0300

    Adding Suspense

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -4,8 +4,6 @@ interface MoviesProps {
   results: [];
 }
 const Movies: React.FC<MoviesProps> = ({ results }) => {
-  console.log(results);
-
   return (
     <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 max-w-6xl mx-auto py-4">
       {results.map((item, index) => (
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { Suspense } from "react";
 import NavbarItems from "./NavbarItems";
 
 const Navbar = () => {
   return (
-    <div className="flex gap-x-3 items-center justify-center mx-auto md:textlg bg-amber-200 dark:bg-gray-600 p-3 ">
-      <NavbarItems title="Trending" param="fetchTrending" />
-      <NavbarItems title="Top Rated" param="fetchTopRated" />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex gap-x-3 items-center justify-center mx-auto md:textlg bg-amber-200 dark:bg-gray-600 p-3 ">
+        <NavbarItems title="Trending" param="fetchTrending" />
+        <NavbarItems title="Top Rated" param="fetchTopRated" />
+      </div>
+    </Suspense>
   );
 };
 
