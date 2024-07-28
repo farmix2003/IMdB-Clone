ebedb58818895ac3476b3a19429e1c1c15d79c4b
Farmix2003 <ffarrux386@gmail.com>
2024-07-28 18:19:22 +0300

    Package updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Provider>
           <Header />
+
           <Navbar />
           <SearchBar />
           {children}
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,12 +3,12 @@ import NavbarItems from "./NavbarItems";
 
 const Navbar = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="flex gap-x-3 items-center justify-center mx-auto md:textlg bg-amber-200 dark:bg-gray-600 p-3 ">
+    <div className="flex gap-x-3 items-center justify-center mx-auto md:textlg bg-amber-200 dark:bg-gray-600 p-3 ">
+      <Suspense fallback={<div>Loading...</div>}>
         <NavbarItems title="Trending" param="fetchTrending" />
         <NavbarItems title="Top Rated" param="fetchTopRated" />
-      </div>
-    </Suspense>
+      </Suspense>
+    </div>
   );
 };
 
